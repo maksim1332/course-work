814e2c573ed9040b91a5f1e05a834ef5039ee78b
USER <mas9n9-mas9n91332@yandex.ru>
2024-10-21 17:17:52 +0500

    Course Work!

---
+++ src/Employee.java
@@ -0,0 +1,9 @@
+public class Employee {
+    String name;
+    double salary;
+
+    Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Employee Book");
+//        String[] listOfEmployees = {"Иванов Иван Иванович", "Мурашкин Юрий Викторовичь", "Байсодыков Руслан Рустамович", "Поряков Никита Иванович", "Кудымов Александр Александровичь"}
+//
+//
+        Employee[] employees = new Employee[5];
+
+        employees[0] = new Employee("Иванов Иван Иванович", 70000);
+        employees[1] = new Employee("Мурашкин Юрий Викторовичь", 60000);
+        employees[2] = new Employee("Байсодыков Руслан Рустамович", 65000);
+        employees[3] = new Employee("Поряков Никита Иванович", 65000);
+        employees[4] = new Employee("Кудымов Александр Александровичь", 15000);
+
+        System.out.println("Список сотрудников:===========");
+        for (Employee emp : employees) {
+            System.out.println(emp.name + ": " + emp.salary);
+        }
+
+        System.out.println("Общая затрата в месяц на всех сотрудников============");
+
+        double totalSalary = 0;
+        for (Employee emp : employees) {
+            totalSalary += emp.salary;
+        }
+        System.out.println("Общая затрата на зп: " + totalSalary);
+
+        System.out.println(" минимальная зп & максимальная зп==========");
+        Employee minSalary = employees[0];
+        Employee maxSalary = employees[0];
+
+        for (Employee emp : employees) {
+            if (emp.salary < minSalary.salary) {
+                minSalary = emp;
+            }
+            if emp
+        }
+
+        System.out.println("Средняя зарплата в месяц===========");
+
+        double averageSalary = totalSalary / employees.length;
+        System.out.println("Средняя зарплата: " + averageSalary);
     }
 }
