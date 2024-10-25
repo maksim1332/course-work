05eff8334cfabeb11f4b19f364cc76614e3192f7
USER <mas9n9-mas9n91332@yandex.ru>
2024-10-25 21:31:18 +0500

    Add EmployeeBook!

--- src/Employee.java
+++ src/Employee.java
@@ -1,16 +1,49 @@
 public class Employee {
-    private static int counter = 0;
-    private int id;
-    String name;
-    double salary;
 
-    Employee(String name, double salary) {
-        this.id = ++counter;
-        this.name = name;
+    private static int idGenerator = 1;
+
+    private final int id;
+    private final String fullName;
+    private int department;
+    private int salary;
+
+    public Employee(String fullName, int department, int salary) {
+        id = idGenerator++;
+        this.fullName = fullName;
+        this.department = department;
         this.salary = salary;
+
     }
 
     public int getId() {
         return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", department=" + department +
+                ", salary=" + salary;
     }
 }
