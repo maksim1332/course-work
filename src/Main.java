812485392e0527cb370265ed3ed40ab1a0662bbc
USER <mas9n9-mas9n91332@yandex.ru>
2024-10-25 21:39:24 +0500

    Add EmployeeBook!

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
     private static void initEmployees() {
         for (int i = 0; i < 10; i++) {
-            String fullName = SURNAMES[RANDOM.nextInt(SURNAMES.length)] + " " +
+            String fullName = SURNAMES[RANDOM.nextInt(SURNAMES.length)]+ " " +
                     NAMES[RANDOM.nextInt(NAMES.length)] + " " +
                     PATRONYMIC_NAMES[RANDOM.nextInt(PATRONYMIC_NAMES.length)];
             Employee employee = new Employee(fullName, RANDOM.nextInt(1, 6), RANDOM.nextInt(50_000, 100_000));
